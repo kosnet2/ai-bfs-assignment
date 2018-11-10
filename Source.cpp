d75cb71cab71320611349fbe970a457276765f6a
Asus ROGN <avogian.karlen@hotmail.com>
2018-11-10 23:24:24 +0200

    finalized project

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-enum struct Status{
-	SUCCESS, FAILURE
-};
-
 template<typename E>
-struct QueueNode{
-	QueueNode<E> *next;
-	QueueNode<E> *parent;
+struct Node{
+	Node<E> *next;
 
-	QueueNode<E>(E entry) {
+	Node<E>(E entry) {
 		this->entry = entry;
 		this->next = nullptr;
 	}
@@ -19,15 +15,15 @@ struct QueueNode{
 
 template<typename E>
 struct Queue {
-	QueueNode<E> *front, *rear;
+	Node<E> *front, *rear;
 	Queue<E>() {
 		this->front = this->rear = nullptr;
 	}
 
-	Status enqueue(const E entry) {
-		QueueNode<E> *tmp = new QueueNode<E>(entry);
+	void enqueue(const E entry) {
+		Node<E> *tmp = new Node<E>(entry);
 		if (tmp == nullptr)
-			return Status::FAILURE;
+			return;
 
 		if (this->rear == nullptr) {
 			this->rear = this->front = tmp;
@@ -36,37 +32,35 @@ struct Queue {
 			this->rear->next = tmp;
 			this->rear = tmp;
 		}
-		return Status::SUCCESS;
 	}
 
 	bool isEmpty() const{
 		return this->front == nullptr;
 	}
 
-	Status getFront(E& val) {
+	bool getFront(E& val) {
 		if (this->isEmpty())
-			return Status::FAILURE;
-		
+			return false;
+
 		val = this->front->entry;
-		return Status::SUCCESS;
+		return true;
 	}
 
-	Status dequeue() {
+	void dequeue() {
 		if (this->isEmpty())
-			return Status::FAILURE;
+			return;
 
-		QueueNode<E> *tmp = this->front;
+		Node<E> *tmp = this->front;
 		this->front = this->front->next;
 
 		if (this->front == nullptr)
 			this->rear = nullptr;
 
 		delete tmp;
-		return Status::SUCCESS;
 	}
 
 	bool includes(const E& val) const {
-		QueueNode<E> *tmp = this->front;
+		Node<E> *tmp = this->front;
 		bool found = false;
 		while (tmp != nullptr && !found) {
 			if (tmp->entry == val)
@@ -78,152 +72,143 @@ struct Queue {
 };
 
 struct FrogState {
-	char configuration[8];
-	int size, emptyIndex;
+	string configuration, message;
+	unsigned int emptyIndex;
 	FrogState *parent;
 
-	FrogState() {
+	FrogState(int stateSize = 7) {
 		this->parent = nullptr;
-		this->size = 8;
-		strcpy_s(configuration, this->size, "BBB_GGG");
-		this->emptyIndex = this->size / 2 - 1;
+		this->configuration = string(stateSize/2,'B') + '_' + string(stateSize/2,'G');
+		this->emptyIndex = this->configuration.size() / 2;
 	}
 
-	FrogState* generateBrown(FrogState parent) {
-		char newConfiguration[8];
+	FrogState(string configuration, FrogState *parent = nullptr, int emptyIndex = -1) {
+		this->parent = parent;
+		this->configuration = configuration;
+		this->emptyIndex = emptyIndex == -1 ? configuration.size() / 2 : emptyIndex;
+	}
+
+	bool generateBrown(FrogState& brownChild, FrogState& parent) {
+		string newConfiguration = this->configuration;
+		string newMessage;
 		int index = this->emptyIndex;
 		bool hasMadeMove = false;
-		strcpy_s(newConfiguration, this->size, this->configuration);
-		
 
 		if (this->emptyIndex > 0 && this->configuration[emptyIndex - 1] == 'B') {
+			newMessage = "Brown frog on place " + to_string(emptyIndex) + " moves right.";
 			newConfiguration[index--] = 'B';
 			newConfiguration[index] = '_';
 			hasMadeMove = true;
 		}
 		else if (this->emptyIndex > 1 && this->configuration[emptyIndex - 1] == 'G' && this->configuration[emptyIndex - 2] == 'B') {
+			newMessage = "Brown frog on place " + to_string(emptyIndex - 1) + " jumps right.";
 			newConfiguration[index--] = 'B';
 			newConfiguration[--index] = '_';
 			hasMadeMove = true;
 		}
 
 		if (hasMadeMove) {
-			FrogState *brownChild = new FrogState();
-			strcpy_s(brownChild->configuration, this->size, newConfiguration);
-			brownChild->emptyIndex = index;
-			brownChild->parent = &parent;
-			return brownChild;
+			brownChild.configuration = newConfiguration;
+			brownChild.message = newMessage;
+			brownChild.parent = &parent;
+			brownChild.emptyIndex = index;
+			return true;
 		}
-		else {
-			return nullptr;
-		}
+		return false;
 	}
 
-	FrogState* generateGreen(FrogState parent) {
-		char newConfiguration[8];
+	bool generateGreen(FrogState& greenChild,FrogState &parent) {
+		string newConfiguration = this->configuration;
+		string newMessage;
 		int index = this->emptyIndex;
 		bool hasMadeMove = false;
 
-		strcpy_s(newConfiguration, this->size, this->configuration);
-		
-		if (this->emptyIndex < this->size - 1 && this->configuration[this->emptyIndex + 1] == 'G') {
+		if (this->emptyIndex < this->configuration.size() - 1 && this->configuration[this->emptyIndex + 1] == 'G') {
+			newMessage = "Green frog on place " + to_string(emptyIndex + 2) + " moves left.";
 			newConfiguration[index++] = 'G';
 			newConfiguration[index] = '_';
 			hasMadeMove = true;
 		}
-		else if (this->emptyIndex < this->size - 2 && this->configuration[this->emptyIndex + 1] == 'B' && this->configuration[this->emptyIndex + 2] == 'G') {
+		else if (this->emptyIndex < this->configuration.size() - 2 && this->configuration[this->emptyIndex + 1] == 'B' && this->configuration[this->emptyIndex + 2] == 'G') {
+			newMessage = "Green frog on place " + to_string(emptyIndex + 3) + " jumps left.";
 			newConfiguration[index++] = 'G';
 			newConfiguration[++index] = '_';
 			hasMadeMove = true;
 		}
 		
-		if (hasMadeMove) {
-			FrogState *greenChild = new FrogState();
-			strcpy_s(greenChild->configuration, this->size, newConfiguration);
-			greenChild->emptyIndex = index;
-			greenChild->parent = &parent;
-			return greenChild;
+		if (hasMadeMove){
+			greenChild.configuration = newConfiguration;
+			greenChild.message = newMessage;
+			greenChild.parent = &parent;
+			greenChild.emptyIndex = index;
+			return true;
 		}
-		else {
-			return nullptr;
-		}
+		return false;
 	}
 
-	friend bool operator==(const FrogState& lhs, const FrogState& rhs) { 
-		for (int i = 0; i < lhs.size; i++) {
-			if (lhs.configuration[i] != rhs.configuration[i])
-				return false;
-		}
-		return true;
-	}
-
-	friend ostream& operator<<(ostream& out, const FrogState *state) {
-		if (state == nullptr)
-			return out << "\n";
-		else
-			return out << state->configuration;
-	}
-
-	void operator=(const FrogState* other) {
-		strcpy_s(this->configuration, other->size, other->configuration);
-		this->parent = other->parent;
-		this->size = other->size;
-		this->emptyIndex = other->emptyIndex;
+	friend bool operator==(const FrogState& lhs, const FrogState& rhs) {
+		return lhs.configuration == rhs.configuration;
 	}
 };
 
-Status BFS(FrogState& solution) {
-	char goalState[] = "GGG_BBB";
-	FrogState *x = &solution;
-	FrogState *goal = new FrogState();
-	strcpy_s(goal->configuration, 8, goalState);
+FrogState BFS(int stateSize) {
+	string goalState = string(stateSize / 2, 'G') + '_' + string(stateSize / 2, 'B');
+	FrogState start(stateSize);
+	FrogState goal(goalState);
 
 	Queue<FrogState> open, closed;
-	open.enqueue(*x);
+	open.enqueue(start);
 
-	while (!open.isEmpty() && x != nullptr) {
-		open.getFront(*x);
-		cout << "Checking : " << x << "  Parent : ";
-		
-		if (x->parent == nullptr)
-			cout << "ROOT";
-		else
-			cout << x->parent;
-		cout << endl;
-		
+	while (!open.isEmpty()) {
+		FrogState *current = new FrogState();
+		open.getFront(*current);
+
 		open.dequeue();
 
-		if (x == goal) {
-			solution = x;
-			return Status::SUCCESS;
+		if (current->configuration == goal.configuration) {
+			return *current;
 		}
 		else {
-			closed.enqueue(*x);
-			FrogState *brownChild = new FrogState();
-			brownChild = x->generateBrown(*x);
-			FrogState *greenChild = new FrogState();
-			greenChild = x->generateGreen(*x);
+			closed.enqueue(*current);
 
-			if (!(open.includes(*brownChild) || closed.includes(*brownChild) || brownChild == nullptr));
-			{
-				open.enqueue(*brownChild);
-			}
-			if (!(open.includes(*greenChild) || closed.includes(*greenChild) || greenChild == nullptr));
-			{
-				open.enqueue(*greenChild);
-			}
+			FrogState brownChild, greenChild;
+			bool brownSuccess = current->generateBrown(brownChild, *current);
+			bool greenSuccess = current->generateGreen(greenChild, *current);
+			
+			if (!(open.includes(brownChild) || closed.includes(brownChild) && brownSuccess))
+				open.enqueue(brownChild);
+			
+			if (!(open.includes(greenChild) || closed.includes(greenChild) && greenSuccess))
+				open.enqueue(greenChild);
 		}
 	}
-	return Status::FAILURE;
+	return start;
+}
+
+void displaySolution(FrogState *solution) {
+	if (solution->parent != nullptr) {
+		displaySolution(solution->parent);
+		cout << solution->message << "\tState : " << solution->configuration<<endl;
+	}
+	else {
+		cout << "Start state : " << solution->configuration << endl << endl;
+		cout << "Jumps as follows :" << endl;
+	}
 }
 
 int main() {
 	
-	FrogState solution;
-	BFS(solution);
-	cout << "SOLUTION\n\n";
-	cout << &solution;
+	int stateSize;
+	cout << "Welcome to frog game solver using BFS. For larger configurations( > 17 ) please find a better algorithm!\n";
+	cout << "Enter state size (odd number) : ";
+	cin >> stateSize;
+	
+	if (stateSize & 0)
+		stateSize++;
+		
+	FrogState solution = BFS(stateSize);
+	displaySolution(&solution);
+
 	cin.get(); cin.get();
 	return 0;
 }
-
